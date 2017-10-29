786e4f6da69e94fcb0ad8f72ac5e1d24e244509c
motoraton <Jero Soler>
2017-10-29 11:28:37 +0100

    Descomprimir archivos con password de NEWPCT

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
 var watch = require('node-watch');
-var path = require('path')
+var path = require('path');
+var getUrlsToArray = require('get-urls-to-array');
+var request = require('request');
+var cheerio = require('cheerio');
 
 var exec = require('child_process').exec;
 var https = require('https');
 
-var directorioNAS = '/home/motoraton/testrar';
+var directorioNAS = '/media/NAS/';
 
 
 
@@ -61,32 +64,73 @@ function file_descomprimir(name) {
 extension = path.extname(name);
 directorio = path.dirname(name);
 
+
   if(extension === ".rar") {
     var orinal_name = path.basename(name);
 
-    name = name.replace(/ /g, '\\ ');
-    directorio = directorio.replace(/ /g, '\\ ');
-
-
-    exec("sudo unrar x "+name+" "+directorio+"", function(error, stdout, stderr) {
-  if (error) {
-
-    console.error('exec error:'+ error);
-    return;
-  }
-
-
-  console.log("Extraido con exito");
+    var contrasena = 0;
+    if(fs.existsSync(directorio+"/CONTRASEÑA PARA DESCOMPRIMIR.txt")) {
+      console.log("HOLA");
+      contrasena = 1;
+    }
 
 
 
+    if(contrasena === 1) {
+      console.log("Tiene contraseña");
+      fs.readFile(directorio+"/CONTRASEÑA PARA DESCOMPRIMIR.txt", "utf8", function(err, data){
+         var url = getUrlsToArray(data);
+         console.log(url[0]);
 
-  console.log('stderr:'+stderr);
-});
+         request(url[0]+"/", function (error, response, html) {
+           if (!error && response.statusCode == 200) {
+             //item_count2++;
+             var $ = cheerio.load(html);
+
+             var password = $("#txt_password").val();
+             console.log(password);
+
+             password = password.replace(/ /g, '\\ ');
+
+             name = name.replace(/ /g, '\\ ');
+             directorio = directorio.replace(/ /g, '\\ ');
+
+
+             exec("unrar x "+name+" "+directorio+" -p"+password, function(error, stdout, stderr) {
+               if (error) {
+                 console.error('exec error:'+ error);
+                 return;
+               }
+               console.log("Extraido con exito");
+               console.log('stderr:'+stderr);
+             });
+           }
+         });
+
+
+      });
+
+
+    } else {
+      console.log("No Tiene contraseña");
+      name = name.replace(/ /g, '\\ ');
+      directorio = directorio.replace(/ /g, '\\ ');
+
+
+      exec("unrar x "+name+" "+directorio+"", function(error, stdout, stderr) {
+        if (error) {
+          console.error('exec error:'+ error);
+          return;
+        }
+        console.log("Extraido con exito");
+        console.log('stderr:'+stderr);
+      });
+    }
 
 
 
 
    }
+
 
 }
